31f94f28f23d4f8809a6e50a230c1c8cd9b0cbe5
c.hao <1219068591@qq.com>
2020-02-07 22:48:26 +0800

    添加test到git

---
+++ test-git/src/com/itheima/git/test.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class test {
+
+        public static void main(String[] args) {
+            System.out.println("hello git!");
+            System.out.println("hello git!");
+            System.out.println("hello git!");
+            System.out.println("hello git!");
+            System.out.println("hello git!");
+            System.out.println("hello git!");
+        }
+
+
+}
